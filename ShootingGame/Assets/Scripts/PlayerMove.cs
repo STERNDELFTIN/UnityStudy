1f31eececa1652db53343479718c59439506a97c
DELFTIN <resendo13@gmail.com>
2024-02-09 16:02:11 +0900

    PlayerMove.cs에 유저 입력 처리 구문 추가 및 플레이어 이동의 또다른 구현법 추가
    1. Input 클래스를 사용하여 사용자 입력 처리 구문 추가 - 가로축 및 세로축 입력 값
    2. 사용자 입력에 따라 방향을 만들어 이동시키기
      Vector3(h, v, 0)
    3. 플레이어 이동 구현2
      Transform.Translate() 함수 사용 대신, 이 함수의 내용을 직접 구현해서 사용하기

--- ShootingGame/Assets/Scripts/PlayerMove.cs
+++ ShootingGame/Assets/Scripts/PlayerMove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float speed = 5; // 플레이어가 이동할 속력
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +14,19 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * 5 * Time.deltaTime);
+        // 사용자 입력
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        // print("h : " + h + "v : " + v);
+
+        // 방향
+        // Vector3 dir = Vector3.right * h + Vector3.up * v;
+        Vector3 dir = new Vector3(h, v, 0);
+        // transform.Translate(dir * speed * Time.deltaTime);
+        transform.position += dir * speed * Time.deltaTime;
+
+        // 이동
+        // transform.Translate(Vector3.right * speed * Time.deltaTime);
         // transform.Translate(Vector3.left * 5 * Time.deltaTime);
         // right, left, up, down, forward, back
     }
