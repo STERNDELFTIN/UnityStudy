e1caeae11defd30c24ba07a7604b6f71d975bbc5
DELFTIN <resendo13@gmail.com>
2024-02-17 00:08:19 +0900

    적 자동 생성
    적 생성 시간을 'UnityEngine.Random.Range()' 함수를 사용하여 설정
    - 처음 태어날 때의 적 생성 시간을 설정하기 위해서 'Start()' 함수 내부에 작성
    - 처음 생성 이후에 'Update()' 함수 내부에 코드를 작성함으로써 적의 생성 시간 계속해서 다시 설정

--- ShootingGame/Assets/Scripts/EnemyManager.cs
+++ ShootingGame/Assets/Scripts/EnemyManager.cs
@@ -11,6 +11,18 @@ public class EnemyManager : MonoBehaviour
     // 적 공장
     public GameObject enemyFactory;
 
+    // 최소 시간
+    float minTime = 1;
+    // 최대 시간
+    float maxTime = 5;
+
+    void Start()
+    {
+        // 태어날 때 적의 생성 시간 설정
+        // 태어날 때 한 번 설정하고 적이 태어날 때 다시 설정
+        createTime = UnityEngine.Random.Range(minTime, maxTime);
+    }
+
     void Update()
     {
         // 시간이 흐름
@@ -27,6 +39,9 @@ public class EnemyManager : MonoBehaviour
             // 위의 코드까지만 작성하면 현재 시간이 일정 시간이 되고 난 후 계속 일정 시간보다 크기 때문에 적을 계속 생성
             // 따라서 아래 코드를 삽입하여 현재 시간이 일정 시간보다 커졌을 때 현재 시간을 0으로 초기화
             currentTime = 0;
+
+            // 적 생성 후 적의 생성 시간을 다시 설정
+            createTime = UnityEngine.Random.Range(minTime, maxTime);
         }
     }
 }
