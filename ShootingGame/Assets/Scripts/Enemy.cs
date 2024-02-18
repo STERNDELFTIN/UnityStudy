57c66c6191b41c491ec9271a5967894e30833426
DELFTIN <resendo13@gmail.com>
2024-02-18 12:23:01 +0900

    Enemy find the player position.
    1. 적은 30% 확률로 플레이어 방향으로, 나머지 확률은 아래 방향으로 이동
      - 0~9 수 중 랜덤으로 하나의 수가 주어짐 UnityEngine.Random.Range(0, 10)
      - 3보다 작으면 플레이어 방향으로, 그렇지 않으면 아래 방향으로 이동 if ~ else문 사용
      - '.Normalize()'를 사용하여 벡터의 정규화를 시킴 (벡터의 크기를 1로 만들어줘 방향에 따른 이동속도를 같게 만들어줌)
     2. 태어날 때 방향을 정하고 그 방향으로 계속 이동하게 만들기
      - Start과 Update문에 지역변수이고 방향을 의미하는 'dir' 변수를 전역 변수로 만들어줌
      - Start과 Update문에서 각각 지역변수인 'dir' 변수 삭제

--- ShootingGame/Assets/Scripts/Enemy.cs
+++ ShootingGame/Assets/Scripts/Enemy.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed = 5;
+    Vector3 dir; // 방향을 전역 변수로 만들어 Start와 Update에서 사용
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Vector3 dir; // 삭제
+        // 0부터 9까지 10개의 값 중에 하나를 랜덤으로 가져옴
+        int randValue = UnityEngine.Random.Range(0, 10);
+        // 만약 3보다 작다면 플레이어 방향
+        if (randValue < 3)
+        {
+            // 플레이어를 찾아 target으로 함
+            GameObject target = GameObject.Find("Player");
+            // 방향을 구하고 싶음. target - me
+            dir = target.transform.position - transform.position;
+            // 방향의 크기를 1로
+            dir.Normalize();
+        }
+        // 그렇지 않다면 아래 방향
+        else dir = Vector3.down;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = Vector3.down;
+        // Vector3 dir = Vector3.down; // 삭제
 
         // 이동 P = P0 + vt
         transform.position += dir * speed * Time.deltaTime;
